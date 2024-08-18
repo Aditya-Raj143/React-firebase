acd802313db6bce9b16843a219a55502bfa6f166
Aditya-Raj143 <adityaxraj72004@gmail.com>
2024-08-18 20:11:17 +0530

    add a feature for posts viewing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Main } from './pages/main'
+import { Main } from './pages/main/main'
 import { Navbar } from './components/navbar';
 import { Login } from './pages/login'
 import { CreatePost } from './pages/create-post/createpost';
---
+++ src/pages/main/main.tsx
@@ -0,0 +1,32 @@
+import { collection, getDocs } from "firebase/firestore"
+import { db } from "../../config/firebase"
+import { useEffect, useState } from "react";
+import { Post } from "./post";
+
+export interface Post {
+    id: string;
+    userId: string;
+    title: string;
+    username: string;
+    description: string;
+}
+
+export const Main = () => {
+    const [postslist, setpostslist] = useState<Post[] | null>(null);
+
+    const postsref = collection(db, "posts");
+
+    const getposts = async () => {
+        const data = await getDocs(postsref)
+        setpostslist(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as Post[]);
+    }
+
+    useEffect(() => {
+        getposts();
+    }, []);
+    return (
+        <div>
+            {postslist?.map((post) => (<Post post={post} />))}
+        </div>
+    )
+}
